dcf60c1c77c8d6a4c55ff6ba28658f5f32de6ac5
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-11-21 07:04:21 +0530

    revised concept

--- 5.loops/sumN.java
+++ 5.loops/sumN.java
@@ -2,14 +2,27 @@ import java.util.*;
 
 public class sumN{
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int sum = 0;
-        int i = 1;
-        while(i <=a){
-            sum = sum+i;
-            i++;
-        }
-        System.out.print("The sum is:" + sum);
+    //     Scanner sc = new Scanner(System.in);
+    //     int a = sc.nextInt();
+    //     int sum = 0;
+    //     int i = 1;
+    //     while(i <=a){
+    //         sum = sum+i;
+    //         i++;
+    //     }
+    //     System.out.print("The sum is:" + sum);
+    // }
+
+    Scanner sc = new Scanner (System.in);
+    int n = sc.nextInt();
+
+    int sum = 0;
+    int i = 1;
+
+    while (i<= n){
+        sum+= i;
+        i++;
+    }
+    System.out.println("The sum is: " + sum);
     }
 }
--- 5.loops/while1.java
+++ 5.loops/while1.java
@@ -3,9 +3,11 @@ import java.util.*;
 public class while1{
     public static void main(String args[]){
         int counter = 0;
-        while(counter < 100){
-            System.out.println("Hello World");
-            counter ++;
+        while (counter < 100){
+            System.out.println("I Love Java Programming");
+            counter++;
         }
+        System.out.println("Loop Ended" );
     }
 }
+

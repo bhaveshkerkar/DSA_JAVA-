b74eecddce185eb976e5c41febaee2d767259ab2
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-11-25 05:48:20 +0530

    update logic

--- 5.loops/primeOrnot.java
+++ 5.loops/primeOrnot.java
@@ -2,6 +2,24 @@ import java.util.*;
 
 public class primeOrnot {
     public static void main(String args[]) {
-        if(i = 2; i< =n -1; i ++)
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+
+        if(n == 2){
+            System.out.println("n is prime");
+        } else {
+            boolean isPrime = true;
+            for (int i = 2; i <= n-1; i ++ ){
+                if (n % i == 0){
+                    isPrime = false;
+                    
+            }
+        }
+        if (isPrime == true){
+            System.out.println("n is prime");
+                } else {
+                    System.out.println("n is not prime");
+                }
+        }
     }
 }

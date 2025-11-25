b814f277ffbb77b51c376be9989803b6567ee4d7
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-11-25 05:54:13 +0530

    update logic for sqrt

--- 5.loops/primeOrnot.java
+++ 5.loops/primeOrnot.java
@@ -9,7 +9,8 @@ public class primeOrnot {
             System.out.println("n is prime");
         } else {
             boolean isPrime = true;
-            for (int i = 2; i <= n-1; i ++ ){
+            // for (int i = 2; i <= n-1; i ++ )
+            for (int i = 2; i <= Math.sqrt(n); i ++ ){
                 if (n % i == 0){
                     isPrime = false;
                     

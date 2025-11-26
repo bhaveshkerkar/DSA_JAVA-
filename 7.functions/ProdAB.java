631869a841eb3ba4f97857a1290b3b01f64ccda4
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-11-26 06:27:34 +0530

    add new logic for product of A & B

---
+++ 7.functions/ProdAB.java
@@ -0,0 +1,11 @@
+public class ProdAB {
+    public static void prodAB(int a, int b){
+        int product = a * b;
+        System.out.println("Product of " + a + " and " + b + " = " + product);
+    }
+
+    public static void main(String[] args) {
+        prodAB(4, 5);
+    }
+    
+}

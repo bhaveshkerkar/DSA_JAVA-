64a656afeb3cb31b8ecff3226afc0ed8e42ff9e3
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-11-29 00:30:15 +0530

    add

--- 7.functions/PrimesInRange.java
+++ 7.functions/PrimesInRange.java
@@ -17,7 +17,7 @@ public static boolean isPrime(int n ){
     public static void PrimesInRange(int n){
         for (int i = 2; i<= n; i++){
             if(isPrime(i)){
-                System.out.print(i + " ");
+                System.out.print(i + "  ");
             }
         }
             System.out.println();

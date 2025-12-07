74d8a4865d22f88de68598b44cf4b64435f74abd
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-12-07 23:59:47 +0530

    edit file

--- 8.patterns-2/ZeroOne_triangle.java
+++ 8.patterns-2/ZeroOne_triangle.java
@@ -3,7 +3,7 @@ public class ZeroOne_triangle {
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= i; j++){
                 if((i + j) % 2 == 0){
-                    System.out.print("1 ");
+                    System.out.print("11 ");
                 } else {
                     System.out.print("0  ");
                 }

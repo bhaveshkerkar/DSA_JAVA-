e335706f91cc10751e17218c5dc699dfb8fc82fc
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-12-08 23:58:45 +0530

    edit file

--- 6.patterns/starp2.java
+++ 6.patterns/starp2.java
@@ -3,7 +3,7 @@ public class starp2{
         int n = 4;
         for(int line = 1; line<=n; line ++){
             for(int star=1; star<=n-line+1; star ++){
-                System.out.print("*");
+                System.out.print("**");
             }
             System.out.println();
         } 

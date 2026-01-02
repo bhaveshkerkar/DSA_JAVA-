79ab9a09955def8b70186ddab99c6a168f834124
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2026-01-02 23:40:48 +0530

    add logiv

---
+++ Problems/Array/reverseInt.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class reverseInt {
+    public int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+
+            if (rev > Integer.MAX_VALUE / 10 || 
+               (rev == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
+
+            if (rev < Integer.MIN_VALUE / 10 || 
+               (rev == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0;
+            }
+
+            rev = rev * 10 + digit;
+            x = x / 10;
+        }
+
+        return rev;
+    }
+    public static void main(String args[]){
+        reverseInt obj = new reverseInt();
+        int result = obj.reverse(-123);
+        System.out.println(result);
+    }
+}
+

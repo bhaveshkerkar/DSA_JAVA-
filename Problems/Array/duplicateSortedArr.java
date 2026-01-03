893d456bbf40a52e50800f40da8f2cacf321198d
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2026-01-03 23:52:04 +0530

    add leetcode 26program

--- Problems/Array/duplicateSortedArr.java
+++ Problems/Array/duplicateSortedArr.java
@@ -3,7 +3,7 @@ package Problems.Array;
 public class duplicateSortedArr {
     class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums.length == 0) return 0;
+        if (nums.length == 1) return 0;
 
         int i = 0;
         for (int j = 1; j < nums.length; j++) {

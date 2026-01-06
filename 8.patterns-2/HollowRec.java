49f589a4893cb90d9d7f8462d4e632cb9c4ad9ca
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2026-01-06 23:55:56 +0530

    edit file"

--- 8.patterns-2/HollowRec.java
+++ 8.patterns-2/HollowRec.java
@@ -3,7 +3,7 @@ public class HollowRec {
         for(int i = 1; i <= totRows; i++){
             for (int j = 1; j <= totCols; j++){
                 if(i == 1 || i == totRows || j == 1 || j == totCols){
-                    System.out.print("*");
+                    System.out.print("**");
                 } else {
                     System.out.print(" ");
                 }

77feafdf82b407c78824528e738d8e25afa8b560
Bhavesh Kerkar <bhaveshkerkar9@gmail.com>
2026-04-17 22:07:17 +0530

    largest number

--- 9.Arrays/LargestNumber.java
+++ 9.Arrays/LargestNumber.java
@@ -3,9 +3,19 @@ import java.util.*;
 public class LargestNumber {
 
     public static int largestNumber(int numbers[] ){
+            int largest = Integer.MIN_VALUE;
+
+            for( int i = 0; i<numbers.length; i++){
+                if( largest < numbers[i]){
+                  largest = numbers[i];
+            }
+        }
+
+        return largest;
 
     }
     public static void main(String[] args) {
-        int numbers = {2, 4, 6, 3 ,1};
+        int numbers[] = {2, 4, 6, 3 ,1};
+        System.out.println("The Largest number is " + largestNumber(numbers));
     }
 }

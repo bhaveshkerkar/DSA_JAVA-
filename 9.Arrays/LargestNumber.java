9ac4d30feccba5bc4cf9ef37db72d639c9c26c73
Bhavesh Kerkar <bhaveshkerkar9@gmail.com>
2026-04-17 21:45:17 +0530

    new problem largest number

---
+++ 9.Arrays/LargestNumber.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class LargestNumber {
+
+    public static int largestNumber(int numbers[] ){
+
+    }
+    public static void main(String[] args) {
+        int numbers = {2, 4, 6, 3 ,1};
+    }
+}

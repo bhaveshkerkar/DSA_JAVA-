bd6baee1ef273373b70be707823420b34fe01667
Bhavesh Kerkar <bhaveshkerkar9@gmail.com>
2026-04-17 20:57:50 +0530

    edit file with further operations

--- 9.Arrays/ArrayCC.java
+++ 9.Arrays/ArrayCC.java
@@ -1,13 +1,8 @@
 import java.util.*;
 
 public class ArrayCC {
-    public static void main(String[] args) {
-        int marks[] = new int[100];
-
-
-        System.out.println("Lenght of an Array: " + marks.length);
-
-
+        // int marks[] = new int[100];
+        // System.out.println("Lenght of an Array: " + marks.length);
         // Scanner sc = new Scanner(System.in);
         //  marks[0] = sc.nextInt();
         //  marks[1] = sc.nextInt();
@@ -20,5 +15,22 @@ public class ArrayCC {
 
         //  int percentage = (marks[0] + marks[1] + marks[2]) / 3;
         //  System.out.println("Percentage: " + percentage + "%");
+        
+
+            public static void Update(int marks[]) {
+                for (int i = 0; i < marks.length; i++){
+                    marks[i] = marks[i] + 1;
+
+                }
+            }
+    public static void main(String[] args) {
+
+        int marks[]  = { 98 , 99 ,97};
+        Update(marks);
+
+        //printing the updated marks
+        for(int i = 0; i < marks.length; i++){
+            System.out.println("Updated Marks : " + marks[i]);
+        }
     }
 }

d8f7a50933b24202cc14e6d8cf20d75992bb6f7f
Bhavesh Kerkar <bhaveshkerkar9@gmail.com>
2026-04-18 23:41:51 +0530

    edit file

--- 9.Arrays/LargestNumber.java
+++ 9.Arrays/LargestNumber.java
@@ -4,18 +4,22 @@ public class LargestNumber {
 
     public static int largestNumber(int numbers[] ){
             int largest = Integer.MIN_VALUE;
+            int smallest = Integer.MAX_VALUE;
 
             for( int i = 0; i<numbers.length; i++){
                 if( largest < numbers[i]){
                   largest = numbers[i];
             }
+            if (smallest > numbers[i]){
+                smallest = numbers[i];
+            }
         }
-
+        System.out.println("Smallest Value is : " + smallest );
         return largest;
 
     }
     public static void main(String[] args) {
         int numbers[] = {2, 4, 6, 3 ,1};
-        System.out.println("The Largest number is " + largestNumber(numbers));
+        System.out.println("The Largest number is : " + largestNumber(numbers));
     }
 }
